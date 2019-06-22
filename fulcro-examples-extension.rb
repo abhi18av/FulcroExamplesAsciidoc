85a2f198e205299ed7c3cde1defee495f09da4fa
Abhinav Sharma <abhi18av@gmail.com>
2019-06-22 22:01:09 +0530

    the html renders correctly - now working on the asciidoc macro call

--- fulcro-examples-extension.rb
+++ fulcro-examples-extension.rb
@@ -15,15 +15,24 @@ class FulcroExamplesBlockMacro < Extensions::BlockMacroProcessor
 
   def process parent, example_name, attrs
 
-    title_html = (attrs.has_key? 'title') ? %(<div class="title">#{attrs['title']}</div>\n) : nil
+    code_content = File.read("./src/#{example_name}.cljs")
 
+    code_html = %(
+
+<div class="listingblock source">
+<div class="content">
+<pre class="highlight"><code class="language-clojure" data-lang="clojure">
+#{code_content}
+</code>
+</pre>
+</div>
+</div>)
 
     html_example = %(
-#{title_html}
-
-<button class="inspector" onClick="book.main.focus('VICTORY')">Focus Inspector</button>
-<div class="short narrow example" id="VICTORY"></div>
+<button class="inspector" onClick="book.main.focus('#{example_name}')">Focus Inspector</button>
+<div class="short narrow example" id="#{example_name}"></div>
 <br/>
+#{code_html}
 )
     create_pass_block parent, html_example, subs: nil
   end
