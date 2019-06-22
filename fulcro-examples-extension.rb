581fc581679473429f019ef7d377c2babbdca655
Abhinav Sharma <abhi18av@gmail.com>
2019-06-22 22:47:56 +0530

    the MVP is done

--- fulcro-examples-extension.rb
+++ fulcro-examples-extension.rb
@@ -4,9 +4,11 @@ include Asciidoctor
 #
 # Usage
 #
-#   gist::12345[]
-#   example::victory_example[]
+#   example::[example_name, example_id, example_source]
+#   example::[victory_name, victory_id, ./src/victory_example.cljs]
 #
+# NOTE: Possible Improvements
+# - take short/narrow as arguments
 
 class FulcroExamplesBlockMacro < Extensions::BlockMacroProcessor
   use_dsl
@@ -15,25 +17,31 @@ class FulcroExamplesBlockMacro < Extensions::BlockMacroProcessor
 
   def process parent, example_name, attrs
 
-    code_content = File.read("./src/#{example_name}.cljs")
+    example_name = attrs[1]
+    example_id = attrs[2]
+    example_source = attrs[3]
 
-    code_html = %(
 
+    clojure_code = File.read(example_source)
+
+    example_block_html = %(
+<div class="exampleblock">
+<div class="title">Example <a id="#{example_name}"></a><a href="##{example_name}">#{example_name}</a></div>
+<div class="content">
+<button class="inspector" onclick="book.main.focus('#{example_id}')">Focus Inspector</button>
+<div class="short narrow example" id="#{example_id}"></div>
+<br>
 <div class="listingblock source">
 <div class="content">
-<pre class="highlight"><code class="language-clojure" data-lang="clojure">
-#{code_content}
-</code>
-</pre>
+  <pre class="highlight">
+    <code class="language-clojure" data-lang="clojure"> #{clojure_code}
+    </code>
+  </pre>
 </div>
-</div>)
-
-    html_example = %(
-<button class="inspector" onClick="book.main.focus('#{example_name}')">Focus Inspector</button>
-<div class="short narrow example" id="#{example_name}"></div>
-<br/>
-#{code_html}
+</div>
+</div>
+</div>
 )
-    create_pass_block parent, html_example, subs: nil
+    create_pass_block parent, example_block_html, subs: nil
   end
 end
