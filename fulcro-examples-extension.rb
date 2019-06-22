2438e1513a65b807a0aa77d919c30c98122356b3
Abhinav Sharma <abhi18av@gmail.com>
2019-06-22 20:32:08 +0530

    at this point we need to fine tune what goes inside the HTML

--- fulcro-examples-extension.rb
+++ fulcro-examples-extension.rb
@@ -1,26 +1,33 @@
 include Asciidoctor
 
-# A block macro that embeds a Gist into the output document
+# A block macro that embeds a Fulcro example into the output document
 #
 # Usage
 #
 #   gist::12345[]
+#   example::victory_example[]
 #
+
 class FulcroExamplesBlockMacro < Extensions::BlockMacroProcessor
   use_dsl
 
   named :example
 
-  def process parent, target, attrs
-    title_html = (attrs.has_key? 'title') ?
-      %(<div class="title">#{attrs['title']}</div>\n) : nil
+  def process parent, example_name, attrs
 
-    html = %(<div class="openblock gist">
-#{title_html}<div class="content">
-<script src="https://gist.github.com/#{target}.js"></script>
-</div>
-</div>)
-
-    create_pass_block parent, html, attrs, subs: nil
+    html_example = %(
+.[[VICTORY]]<<{VICTORY}>>
+====
+++++
+<button class="inspector" onClick="book.main.focus('VICTORY')">Focus Inspector</button>
+<div class="short narrow example" id="VICTORY"></div>
+<br/>
+++++
+[source,clojure,role="source"]
+----
+include:: "./src/#{example_name}.cljs"[]
+----
+)
+    create_pass_block parent, html_example, subs: nil
   end
 end
