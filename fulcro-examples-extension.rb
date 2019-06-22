42759d7aad3809fb9914d31f87d7205705b12cc9
Abhinav Sharma <abhi18av@gmail.com>
2019-06-22 21:31:30 +0530

    done with rendering of fulcro inspector

--- fulcro-examples-extension.rb
+++ fulcro-examples-extension.rb
@@ -15,7 +15,12 @@ class FulcroExamplesBlockMacro < Extensions::BlockMacroProcessor
 
   def process parent, example_name, attrs
 
+    title_html = (attrs.has_key? 'title') ? %(<div class="title">#{attrs['title']}</div>\n) : nil
+
+
     html_example = %(
+#{title_html}
+
 <button class="inspector" onClick="book.main.focus('VICTORY')">Focus Inspector</button>
 <div class="short narrow example" id="VICTORY"></div>
 <br/>
